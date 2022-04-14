4604f1e011b799991245ebad4d8f128b183767df
KyleJKells <thetoppumpkin@gmail.com>
2022-04-13 22:22:39 -0600

    Added navbar with profile info

--- adv-spotify-search/src/components/Navbar.jsx
+++ adv-spotify-search/src/components/Navbar.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
-
+import React from 'react';
+import styled from 'styled-components';
+import { CgProfile} from "react-icons/cg"
+import { useStateProvider } from '../utils/StateProvider';
 export default function Navbar() {
+  const [{userInfo}] = useStateProvider();
   return (
-    <div>Navbar</div>
-  )
+    <Container>
+      <div className='avatar'>
+        <a href='#'>
+          <CgProfile/>
+          <span>{userInfo?.userName}</span>
+        </a>
+      </div>
+    </Container>
+
+    
+  ); 
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 1 rem;
+  position: sticky;
+  top:0 ;
+  background-color: black;
+  
+  .avatar {
+    background-color:white;
+    padding: .6rem;
+    
+    a{
+      text-decoration: none;
+      color: black;
+      font-weight: bold;
+      font-size: 1.5rem;
+      svg{
+        font-size: 2rem;
+        padding-right: 0.5rem;
+      }
+    }
+    
+   
+    
+    
+  }
+`;
